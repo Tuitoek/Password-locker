c40ff8d8e89237f72f472d6fa123e32136c4c7de
Sarah Tuitoek <sjtoek@gmail.com>
2019-01-27 21:16:55 +0300

    Updated user class

--- user.py
+++ user.py
@@ -11,19 +11,19 @@ class User:
 
     def __init__(self,first_name,last_name,email,username):
 
-        '''
-          __init__ method that helps us define properties for our objects.
+            '''
+                __init__ method that helps us define properties for our objects.
 
-        Args:
-            first_name : New user first name.
-            last_name : New user last name.
-            email : New user email address.
-            username : New user username.
+                Args:
+                    first_name : New user first name.
+                    last_name : New user last name.
+                    email : New user email address.
+                    username : New user username.
 
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.username = username
+                self.first_name = first_name
+                self.last_name = last_name
+                self.email = email
+                self.username = username
 
 
-        '''
+            '''
