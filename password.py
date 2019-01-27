85c3f5c7b807f89cbde93e54c437989e6e9a6ea1
Sarah Tuitoek <sjtoek@gmail.com>
2019-01-27 20:31:55 +0300

    Add user class

---
+++ password.py
@@ -0,0 +1,22 @@
+class User:
+
+   pass
+   user_list=[];   
+
+    Class that generates new instances for users.
+
+
+    def __init__(self,first_name,last_name,email,username):
+
+        ...
+        Args:
+            first_name:New user first name.
+            last_name:New user last name.
+            email:New user email address.
+            username:New user username.
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.username = username
+        ...
