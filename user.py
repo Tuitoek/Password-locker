919a0979b8d7b33d6dc77b14888a218fe70d6036
Sarah Tuitoek <sjtoek@gmail.com>
2019-01-27 20:57:13 +0300

    Add user class to user.py

---
+++ user.py
@@ -0,0 +1,29 @@
+class User:
+
+    '''
+    Class for generating instances for user.
+    '''
+
+
+    pass
+    user_list=[];
+
+
+    def __init__(self,first_name,last_name,email,username):
+
+        '''
+          __init__ method that helps us define properties for our objects.
+
+        Args:
+            first_name : New user first name.
+            last_name : New user last name.
+            email : New user email address.
+            username : New user username.
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.username = username
+
+
+        '''
