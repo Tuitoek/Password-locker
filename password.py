1d7cedda0783e82340812af394bfb0f8c2944942
Sarah Tuitoek <sjtoek@gmail.com>
2019-01-27 20:55:36 +0300

    removed the user class and updated the password.py file

--- password.py
+++ password.py
@@ -1,41 +1,14 @@
-class User:
-
-    Class for generating instances for user.
-    
-
-    pass
-    user_list=[];
-
-
-    def __init__(self,first_name,last_name,email,username):
-
-        ...
-          __init__ method that helps us define properties for our objects.
-
-        Args:
-            first_name : New user first name.
-            last_name : New user last name.
-            email : New user email address.
-            username : New user username.
-
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.username = username
-
-
-        ...
-
 class Credentials:
-
-    Class for generating instances for credentials.
-
+    
+    """
+    Class that generates instances for credentials.
+    """
     pass
     credential_list = [];
 
     def __init__(self,account_name,password):
 
-    ...
+    '''
 
     Args:
       account_name : New  credentials account name.
@@ -43,7 +16,7 @@ class Credentials:
 
     self.account_name = account_name
     self.password = password   
-    ... 
+    ''' 
 
     credential_list = []; 
 
