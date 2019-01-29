dc025837dc6d48a8f11584f15c9d9522ee3df941
Sarah Tuitoek <sjtoek@gmail.com>
2019-01-29 10:28:11 +0300

    Updated user.py

--- user.py
+++ user.py
@@ -9,8 +9,8 @@ class User:
     '''
 
 
-    pass
-    user_list=[];
+
+    user_list=[]
 
 
     def __init__(self,first_name,last_name,email,username):
@@ -31,106 +31,80 @@ class User:
 
 
             '''
+            self.first_name = first_name
+            self.last_name = last_name
+            self.email = email
+            self.username = username
 
-    def register_user(self):
+    def save_user(self):
         """
         method that registers and saves objects into our user_list
-        """        
+        """
         User.user_list.append(self)
 
-    def login_user(self):
-        """
-        method that allows one to access their own personal account
-
-        """
-        if (username == user_name and password == password):
-
-           print("Log in successful")
 
 class Credential:
 
     """
     Class that generates instances for credentials.
     """
-    pass
-    credential_list = [];
-    user_credential_list = [];
+
+    credential_list = []
 
     def __init__(self,account_name,password):
 
             """
 
             Args:
-                account_name : New  credentials account name.
-                password : New credentials password.
+                account_name : New  credential account name.
+                password : New credential password.
 
             self.account_name = account_name
-            self.password = password 
-                
+            self.password = password
 
-            """ 
 
-    def __init__(self,account_name,user_name,password):  
+            """
+            self.account_name = account_name
+            self.password = password
 
-        """
-
-        Args:
-            account_name: New user credentials account.
-            user_name:New user credentials username.
-            password:new user credentials password.
-
-        self.user_name = user_name
-        self.password = password 
-
-        """  
     def create_credentials(self):
         """
         For creating a new credential account
 
-        """   
-        Credential.user_credential_list.append(self)
-
-    def save_credentials(self):
         """
-        Allows the user to save their credentials
-        """  
-        Credential.user_credential_list.append(self)
+        Credential.credential_list.append(self)
 
     def  pass_gen(size = 8,chars=string.ascii_letters + string.digits + string.punctuation):
-        return ''.join(random.choice(chars)for _ in range(size))   
+        return ''.join(random.choice(chars)for _ in range(size))
+
+
+    def save_credential(self):
+        """
+        Allows the user to save their credentials
+        """
+        Credential.credential_list.append(self)
+
+    # @classmethod
+    # def find_by_account_name(cls,account_name):
+    #     """
+    #     method returns the credential list
+    #
+    #     """
+    #     for credential in cls.user_list:
+    #         if user.account_name == account_name:
+    #             return credential
 
     def delete_credentials(self):
         """
         Enables the user to delete some credentials
 
         """
-        Credential.user_credential_list.remove(self)
+        Credential.credential_list.remove(self)
 
     @classmethod
-    def display_all_credentials(cls):
+    def display_credentials(cls):
         """
         returns the User's credential list
 
         """
-        return cls.user_credential_list
-
-if __name__ == '__main__' :
-    unittest.main()       
-
-    
-    
-
-
-
-
-
-    
-
-       
-
-    
-
-
-
-
-    
+        return cls.credential_list
